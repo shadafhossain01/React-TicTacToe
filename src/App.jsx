4064673fa1b98f39d938ad8305efc1cc80b606e1
Shadaf Hossain <shadafhossain01@gmail.com>
2025-04-26 01:44:27 +0600

    Half Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react"
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [box,setBox]=useState(Array(9).fill(""))
+  const [isX,setX]=useState(true)
+  const [gameOver,setGameOver]=useState(false)
+  const [winner,setWinner]=useState(null)
+
+  function handleClick(idx){
+    if(box[idx] || winner ) return;
+      const newBoard=[...box]
+    newBoard[idx]=isX?"X":"O"
+    setBox(newBoard)
+    setX(!isX)
+  }
+
+  function winnerChecker(box){
+    const winningPattern=[
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+
+    for(let i=0;i<winningPattern.length;i++){
+      const[a,b,c]=winningPattern[i]
+      if(box[a] && box[a]==box[b] && box[a]==box[c]){
+        setGameOver(true)
+        return box[a];
+      }
+    }
+  }
+
+useEffect(()=>{
+  const winner=winnerChecker(box)
+  setWinner(winner)
+},[box])
+
+function resetGame(){
+  setBox(Array(9).fill(""))
+  setX(true)
+  setGameOver(false)
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>TicTacToe Game</h1>
+      {
+        gameOver? (<>
+          <h5>Winner is {winner} </h5>
+          <button onClick={resetGame}> Restart</button>
+        </>) : (<>
+          <h4>It's now {isX?"X":"O"} turn</h4>
+      <div className="game-board">
+      {
+        box.map((item,idx)=>(
+          <button key={idx} onClick={()=>handleClick(idx)} > {item} </button>
+        ))
+      }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        </>)
+      }
+    </div>
   )
 }
 
